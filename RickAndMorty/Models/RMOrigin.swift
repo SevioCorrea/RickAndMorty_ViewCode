a8d237e31a4c0aad55ffa8328230f00e7303bc6d
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2023-01-02 11:20:22 -0300

    Adding Codable Files

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,14 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Sévio Basilio Corrêa on 02/01/23.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    
+    let name: String
+    let url: String
+}
